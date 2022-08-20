ad094b5818455ff0cb294d088fba04d3bc152ac8
yassmine23 <yasminessam827@gmail.com>
2022-08-20 17:06:20 +0200

    carousel card

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import { StyleSheet, View, FlatList, ScrollView } from 'react-native';
 import Header from './components/header';
 import Footer from './components/footer';
 import Card from './components/card';
+import TabsHeader from './components/tabsHeader';
 
 export default function App() {
   return ( <>
     <View style={styles.header}>
             <Header/>
     </View>
-    <ScrollView style={styles.header} >
-      <Card/>
+    <View style={styles.container}>
+    <Card/></View>           
+    
 
-    </ScrollView>
+    
 
     <View>
       <Footer/>
@@ -24,5 +26,9 @@ const styles = StyleSheet.create({
  
   header:{
     padding:30,
+  },
+  container:{
+    flex:1,
+     alignItems:'center'
   }
 });
--- components/card.js
+++ components/card.js
@@ -1,9 +1,67 @@
-import React from 'react'
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-export default function Card() {
+const { width } = Dimensions.get("window");
+const height = width * 0.6;
+
+const imgs = [
+  "https://a0.muscache.com/im/pictures/be42241a-5346-4745-a2ef-8cf7576f88b8.jpg?im_w=960",
+  "https://a0.muscache.com/im/pictures/4119dad5-30be-4e72-844e-a7343046070c.jpg?im_w=720",
+  "https://a0.muscache.com/im/pictures/b6b95b9b-5281-454d-adaa-af75044cacca.jpg?im_w=720",
+  "https://a0.muscache.com/im/pictures/fbd09e63-43be-4e0c-87e5-bf0eecc16009.jpg?im_w=720",
+  "https://a0.muscache.com/im/pictures/07ca45f5-6dd9-485f-928e-f6e683a1de31.jpg?im_w=720",
+];
+
+export default function MainCard() {
   return (
-    <Text>card</Text>
-  )
+    <>
+      <View style={styles.container}>
+        <ScrollView pagingEnabled horizontal style={styles.scroll}>
+          {imgs.map((image, index) => (
+            <Image key={index} source={{ uri: image }} style={styles.img} />
+          ))}
+        </ScrollView>
+        <View style={styles.pagination}>
+          {imgs.map((i, k) => (
+            <Text style={styles.pagingText}>⬤</Text>
+          ))}
+        </View>
+      </View>
+      <View style={styles.details}>
+        <Text style={{fontWeight:'bold'}}>Harlingen, Friesland, Netherlands</Text>
+        <Text style={{color:'gray'}}>professional Host</Text>
+        <Text style={{color:'gray'}}>18-23 sept</Text>
+        <Text  style={{fontWeight:'bold'}}>$250  <Text  style={{fontWeight:'normal'}}>night</Text></Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { width, height },
+  scroll: { width, height },
+  img: { width, height, resizeMode: "cover" 
+},
+  pagination: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    alignSelf: "center",
+  },
+  pagingText: { 
+    fontSize: 14, 
+    color: "#fff", 
+    margin: 5 
+},
+  details: {
+     width, height,
+     padding: 20 },
+});
